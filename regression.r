b12fd0c39e7ed714665e27b1df208f443822982d
alexanderschaller <alexjschaller@gmail.com>
2023-05-14 10:12:15 +0200

    Regression on R of the data

--- regression.r
+++ regression.r
@@ -2,4 +2,26 @@
 # Accuracy vs PT, FS, EC and PD
 
 # Load the data
-data <- read.csv("
+library(stats)
+library(stargazer)
+
+data <- data.frame(read.csv("data/clean_data.csv"))
+data.iri <- data$PT + data$FS + data$EC + data$PD
+reg.accuracy <- lm(Accuracy ~ PT + FS + EC + PD, data=data)
+reg.hel_accuracy <- lm(HEL.Accuracy ~ PT + FS + EC + PD, data=data)
+reg.hel_accuracy_iri <- lm(HEL.Accuracy ~ iri, data=data)
+reg.lel_accuracy <- lm(LEL.Accuracy ~ PT + FS + EC + PD, data=data)
+reg.lel_accuracy_iri <- lm(LEL.Accuracy ~ iri, data=data)
+reg.hel_accuracy_sex <- lm(HEL.Accuracy ~ PT + FS + EC + PD + Sex, data=data)
+reg.lel_accuracy_sex <- lm(LEL.Accuracy ~ PT + FS + EC + PD + Sex, data=data)
+reg.accuracy_sex <- lm(Accuracy ~ PT + FS + EC + PD + Sex, data=data)
+
+print(summary(reg.accuracy))
+print(summary(reg.hel_accuracy))
+print(summary(reg.lel_accuracy))
+print(summary(reg.hel_accuracy_iri))
+print(summary(reg.lel_accuracy_iri))
+print(summary(reg.hel_accuracy_sex))
+print(summary(reg.lel_accuracy_sex))
+print(summary(reg.accuracy_sex))
+
